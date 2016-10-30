b3cce354f184a47bb446c1ea3bdb09e34132fa9b
Elliot Lewis <elliotlewis@Elliots-MacBook-Air.local>
2016-10-31 02:34:29 +0900

    mitigate need for multiple commands

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # ez-record
-Capture mic/loopback audio and save it to a file.
+Capture mic/loopback audio and save it to a file.  
+Works nicely as an OSX service with keyboard macros...
 Largely plagiarising [this gist](https://gist.github.com/sloria/5693955).
 
-Start:
+Start recording:
+```
+$ python main.py&
+```
 
+Stop recording:
 ```
 $ python main.py
-```
-
-Stop:
-```
-$ echo "stop" > /tmp/recpipe
 ```
--- main.py
+++ main.py
@@ -1,31 +1,46 @@
 import os, time
+import subprocess
 from recorder import Recorder
 
+dir = os.getenv('EZ_REC_PATH')
+if not dir:
+    dir = os.path.dirname(os.path.realpath(__file__)) + '/'
+
 PIPE_PATH = '/tmp/recpipe'
-FILE_NAME = time.strftime("%Y-%m-%d_%H-%M-%S") + '.wav'
+FILE_NAME = dir + time.strftime("%Y-%m-%d_%H-%M-%S") + '.wav'
 # timeout in seconds
-TIMEOUT = 5
+TIMEOUT = 60
+# number of input channels
+CHANNELS = 2
 
-if not os.path.exists(PIPE_PATH):
+def run():
+    if os.path.exists(PIPE_PATH):
+        subprocess.call(['echo "stop" > ' + PIPE_PATH], shell=True)
+        return
+
     os.mkfifo(PIPE_PATH)
-pipe_fd = lambda: os.fdopen(os.open(PIPE_PATH, os.O_RDONLY | os.O_NONBLOCK))
+    pipe_fd = lambda: os.fdopen(os.open(PIPE_PATH, os.O_RDONLY | os.O_NONBLOCK))
 
-rec = Recorder(channels=2)
-write_stream = lambda: rec.open(FILE_NAME, 'wb')
+    rec = Recorder(channels=CHANNELS)
+    write_stream = lambda: rec.open(FILE_NAME, 'wb')
 
-with write_stream() as record, pipe_fd() as pipe:
-    record.start_recording()
-    print('start capture...')
-    start = time.time()
-    while True:
-        try:
-            elapsed = time.time() - start
-            if elapsed > TIMEOUT:
-                print('timed out')
+    with write_stream() as record, pipe_fd() as pipe:
+        record.start_recording()
+        print('start capture...')
+        start = time.time()
+        while True:
+            try:
+                elapsed = time.time() - start
+                if elapsed > TIMEOUT:
+                    print('timed out')
+                    break
+                pipe.read()
+            except IOError: 
                 break
-            pipe.read()
-        except IOError: 
-            break
-    record.stop_recording()
-    print('audio captured!')
+        os.remove(PIPE_PATH)
+        record.stop_recording()
+        print('audio captured!')
+
+if __name__ == '__main__':
+    run()
 
