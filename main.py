6345908fc3a82accc4099435c34f3177a2fc1e0f
Elliot Lewis <elliotlewis@Elliots-MacBook-Air.local>
2016-10-31 01:01:35 +0900

    add timeout functionality and some prints

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from recorder import Recorder
 
 PIPE_PATH = '/tmp/recpipe'
 FILE_NAME = time.strftime("%Y-%m-%d_%H-%M-%S") + '.wav'
+# timeout in seconds
+TIMEOUT = 5
 
 if not os.path.exists(PIPE_PATH):
     os.mkfifo(PIPE_PATH)
@@ -13,10 +15,17 @@ write_stream = lambda: rec.open(FILE_NAME, 'wb')
 
 with write_stream() as record, pipe_fd() as pipe:
     record.start_recording()
+    print('start capture...')
+    start = time.time()
     while True:
         try:
+            elapsed = time.time() - start
+            if elapsed > TIMEOUT:
+                print('timed out')
+                break
             pipe.read()
         except IOError: 
             break
     record.stop_recording()
+    print('audio captured!')
 
